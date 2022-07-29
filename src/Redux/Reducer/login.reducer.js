2669d11ef532c11f60df7ec3c80758366ba8a5d6
nikunjdudhat <nikunjdudhat2002@gmail.com>
2022-07-29 08:14:24 +0530

    fix

--- src/Redux/Reducer/login.reducer.js
+++ src/Redux/Reducer/login.reducer.js
@@ -15,12 +15,7 @@ export const LoginReducer = (state= initialState, action) => {
                 isLoading: false,
                 user: action.payload,
                 error: ''
-            }
-        case ActionType.DECREMENT_COUNTER : 
-            return {
-                ...state,
-                count : state.count - 1
-            }            
+            }       
         default : 
             return state;
     }
